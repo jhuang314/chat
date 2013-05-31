0b7681f29ab709cf4a40cdff279739e3ce86a8fb
Jesse Huang <jh3141@gmail.com>
2013-05-31 16:10:51 -0400

    updated index.js to use jade

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ var express = require("express");
 var app = express();
 var port = 8080;
 
+// setup jade
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+
 app.get("/", function(req, res) {
     res.send("it works!");
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(port);
 console.log("Listening on port " + port);
+
